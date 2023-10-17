a9fe8eaf797794996e09fe9ab45fc94f6eccff32
Robert Connolly <robertoconnolly100@gmail.com>
2023-10-17 11:23:37 +0200

    login y registrar html

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
@@ -6,3 +6,11 @@ auth = Blueprint("auth", __name__)
 @auth.route("/")
 def index():
     return "<h1>Esto es la pagina de login!</h1>"
+
+@auth.route("/login")
+def login():
+    return render_template("login.html")
+
+@auth.route("/registrar")
+def registrar():
+    return render_template("register.html")
