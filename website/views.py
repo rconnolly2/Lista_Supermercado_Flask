550908c4b014cfd35dffd0a5e3b43ea96cf743fd
Robert Connolly <robertoconnolly100@gmail.com>
2023-10-30 14:39:10 +0100

    changed grid jinja2

--- website/views.py
+++ website/views.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, current_app
 
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
+@views.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    bd = current_app.bd
+    cursor = bd.cursor()
+    cursor.execute("SELECT ID, nombre_articulo, precio_kilo, peso_articulo, precio_articulo, marca_articulo FROM Articulo;")
+    datos_artículos = cursor.fetchall()
+    
+    return render_template("index.html", artículos=datos_artículos)
 
 @views.route("/protegido")
 def protegido():
